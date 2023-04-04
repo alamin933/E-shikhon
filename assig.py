fd76fb3fdcc97de062b7c448c4f501ee3fef5b02
alamin <alaminsharkar46@gmail.com>
2023-04-04 22:22:38 +0600

    4th

---
+++ assig.py
@@ -0,0 +1,15 @@
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print(j+1,end="")
+#     print()
+
+# for row in range(5):
+#     for col in range(4,row,-1):
+#         print(" ",end="")
+#     for col in range(row+1):
+#         print("*",end="")
+#     print()
+for i in range(5,0,-1):
+    for j in range(i):
+        print(j+1,end="")
+    print()
