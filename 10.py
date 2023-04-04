c1e117ba5828c9f554f94bdfe30f753695dfaa84
alamin <alaminsharkar46@gmail.com>
2023-04-04 22:21:57 +0600

    3rd commit

---
+++ 10.py
@@ -0,0 +1,23 @@
+# for i in range(9):
+#     print("*")
+# for row in range(0,9,1):
+#     for col in range(0,4,1):
+#         if row==0 or col==0 or row==8 or col==3:
+#             print("*",end="")
+#         else:
+#             print(" ",end="")
+#     print()
+
+
+# for row in range (1,6):
+#     for col in range(5,row,-1):
+#         print(" ",end="")
+#     for col in range(row):
+#         print("*",end="")
+#     print()
+# for row in range (6):
+#     for col in range(5,row,-1):
+#         print("",end="")
+#     for col in range(row):
+#         print("*",end="")
+#     print()
