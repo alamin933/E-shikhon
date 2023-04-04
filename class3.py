6dadee5564ea1796fb8f9ea59f6a1bc804b92bd1
alamin <alaminsharkar46@gmail.com>
2023-04-04 22:24:17 +0600

    6

---
+++ class3.py
@@ -0,0 +1,46 @@
+a = input("what is your name?")
+print(a)
+b = input("how old are you?")
+print(b)
+
+x = 10
+y = 40
+z = x+y
+xx = x/y
+yy = x//y
+zz = x%y
+aa = x**y
+bb = x-y
+cc = x*y
+print(z)
+print(xx)
+print(yy)
+print(aa)
+print(bb)
+print(cc)
+
+# comparision oparator
+x = 40
+y = 60
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print( x<= y)
+print( x>=y )
+
+
+# assignment statement
+
+x = 39
+y = 47
+x =+ y
+print(x)
+x -= y
+print(x)
+x /= y 
+print(x)
+x %=y 
+print(x)
+x //= y
+print(x)
