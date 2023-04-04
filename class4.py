b94d3ba7c07dd975f4b68c94c485aba7f40c303c
alamin <alaminsharkar46@gmail.com>
2023-04-04 22:07:04 +0600

    f commit

---
+++ class4.py
@@ -0,0 +1,51 @@
+# string statement 
+
+# name = "alamin"
+# numOfOne = 22
+# numOfTwo = 44
+# print(type(numOfOne*numOfTwo))
+# print(type(name))
+
+# list
+
+# list = ["hi bangladesh",4.99,'a']
+# list[1] = ["hpp"]
+# print (list)
+# print (type(list))
+
+# tuple
+
+# tuple = ("hello boy",708,'Y',4.00008)
+# print  (tuple)
+# tuple = tuple+ (4,6,7,0,2,5)
+# print (tuple)
+
+# set
+# set = {"hello my book",2,'u',5.000066,"true"}
+# print (set)
+# set.add(1)
+# print (set)
+
+# # integars 
+# a = 5000
+# print (type(a))
+# print(a.bit_length())
+
+# # float
+# a = 4.9944
+# print (a)
+# print (type(a))
+
+# # complex numbers
+# a = 1+ 3j
+# print (type(a))
+
+# # strings
+# g = ("good boy")
+# print (g)
+# print(len(g))
+
+# dictionarys
+a = {"name": "haasan", "age": "23", "city": "dhaka" }
+print(a)
+print(type(a))
