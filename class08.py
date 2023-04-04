dcafb4d80802f8bdcb239922f3c9c9ba98fe478d
alamin <alaminsharkar46@gmail.com>
2023-04-04 22:27:29 +0600

    9

---
+++ class08.py
@@ -0,0 +1,13 @@
+for i in range (8,0,-1):
+    for j in range(1,i):
+        print(j,end="")
+    print()
+
+# for row in range(5):
+#     for col in range(5,row,-1):
+#         print("*",end="")
+#     print()
+# for row in range(6):
+#     for col in range(row):
+#         print("*",end="")
+#     print()
